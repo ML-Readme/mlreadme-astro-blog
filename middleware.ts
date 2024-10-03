fba240d455c7b0e77f67c6de774304586f8842ca
jgeofil <jeremy.geo@gmail.com>
2024-10-02 21:26:41 -0400

    vercel middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+
+import { rewrite } from '@vercel/edge';
+
+export const config = {
+    matcher: ['/admin/:path*'],
+};
+
+export default function middleware(request: Request) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith('/admin')) {
+      return rewrite(new URL('/admin', request.url));
+    }
+}
