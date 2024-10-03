d64fa5f115edec30130c17f40a7d026a9374d214
jgeofil <jeremy.geo@gmail.com>
2024-10-02 21:08:02 -0400

    admin route

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,7 +1,13 @@
 
+
+/** 
 export function GET(context, next) {
     if (context.url.pathname.includes("admin")) {
       return context.rewrite("/admin")
     }
     next()
+}*/
+
+export async function GET({ params, redirect }) {
+  return redirect("/admin", 307);
 }
