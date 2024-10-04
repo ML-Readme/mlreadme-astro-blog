7bb959f931b43e74ff4807fa82e7850ec5bbaa75
jgeofil <jeremy.geo@gmail.com>
2024-10-04 13:22:51 -0400

    portable text base

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
-import {schema} from './src/schemas'
+import {schema} from '@sanity/schemas'
 
 export default defineConfig({
     name: 'ml-readme',
