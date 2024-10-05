78e6fa7fd7e9be2a7e3a8e39c16e1e0f388478b6
jgeofil <jeremy.geo@gmail.com>
2024-10-05 18:40:54 -0400

    sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercelServerless from '@astrojs/vercel/serverless';
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mlread.me',
@@ -25,44 +27,38 @@ export default defineConfig({
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
-  integrations: [
-    mdx(),
-    sitemap({
-      serialize(item) {
-        if (/info/.test(item.url)) {
-          return undefined;
-        }
-        if (/posts/.test(item.url)) { //TODO add function to return image urls to item.image and item.video
-          item.changefreq = 'daily';
-          item.lastmod = new Date();
-          item.priority = 1;
-          return item;
-        }
-        item.changefreq = 'weekly';
+  integrations: [mdx(), sitemap({
+    serialize(item) {
+      if (/info/.test(item.url)) {
+        return undefined;
+      }
+      if (/posts/.test(item.url)) { //TODO add function to return image urls to item.image and item.video
+        item.changefreq = 'daily';
         item.lastmod = new Date();
-        item.priority = 0.9;
+        item.priority = 1;
         return item;
+      }
+      item.changefreq = 'weekly';
+      item.lastmod = new Date();
+      item.priority = 0.9;
+      return item;
+      },
+      i18n: {
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+          es: 'en-UK',
+          fr: 'en-CA',
         },
-        i18n: {
-          defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
-          locales: {
-            en: 'en-US', // The `defaultLocale` value must present in `locales` keys
-            es: 'en-UK',
-            fr: 'en-CA',
-          },
-        }
-      }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sanity({
-      projectId: "fblwvub6",
-      dataset: "production",
-      token: import.meta.env.SANITY_API_WRITE_TOKEN,
-      useCdn: true,
-      apiVersion: "2024-10-01",
-      studioBasePath: '/admin',
-    }),
-    react(),
-  ]
+      }
+    }), tailwind({
+    applyBaseStyles: false,
+  }), sanity({
+    projectId: "fblwvub6",
+    dataset: "production",
+    token: import.meta.env.SANITY_API_WRITE_TOKEN,
+    useCdn: true,
+    apiVersion: "2024-10-01",
+    studioBasePath: '/admin',
+  }), react(), sentry()]
 });
