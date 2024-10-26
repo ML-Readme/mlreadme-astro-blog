083c11120f2faf0966d9236bddb92ca50003196d
jgeofil <jeremy.geo@gmail.com>
2024-10-26 19:09:34 -0400

    feat: Integrate Keystatic for CMS
    Integrates Keystatic as a headless CMS to manage blog content, enabling easier editing and content management workflows.
    
    This provides a more streamlined approach to creating and updating blog posts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,35 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, envField } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
-import { getViteConfig } from 'astro/config';
+import keystatic from '@keystatic/astro'
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
 
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mlread.me',
-	adapter: vercel({
-		edgeMiddleware: true
-	}),
-	root: './',
-	output: 'server',
-	server: { port: 3000, host: true },
-	build: {},
-	integrations: [
-		mdx(),
-		sitemap(),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		//spotlightjs()
-	],
-	image: {
-		domains: [
-			"https://cdn.simpleicons.org"
-		] 
-	}
+    site: 'https://mlread.me',
+    adapter: vercel({
+        edgeMiddleware: true
+    }),
+    root: './',
+    output: 'server',
+    server: { port: 3000, host: "127.0.0.1" },
+    build: {},
+    integrations: [mdx(), sitemap(), keystatic(),
+    tailwind({
+        applyBaseStyles: false
+		}), react(), markdoc()],
+    image: {
+        domains: [
+            "https://cdn.simpleicons.org"
+        ] 
+    },
+    env: {
+        schema: {
+            PUBLIC_KEYSTATIC_GITHUB_APP_SLUG: envField.string({ context: "client", access: "public", optional: false }), 
+        }
+      }
 })
---
+++ keystatic.config.ts
@@ -0,0 +1,25 @@
+// keystatic.config.ts
+import { config, fields, collection } from '@keystatic/core';
+
+const REPO_OWNER = 'jgeofil';
+const REPO_NAME = 'mlreadme-astro-blog';
+
+
+export default config({
+    storage: {
+        kind: 'github',
+        repo: `${REPO_OWNER}/${REPO_NAME}`
+    },
+  collections: {
+    posts: collection({
+      label: 'Short',
+      slugField: 'title',
+      path: 'src/data/shorts/*/',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+  },
+});
