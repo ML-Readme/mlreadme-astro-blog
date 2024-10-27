35d7e20569ac82e9082ca710e80c6a969c82d44b
jgeofil <jeremy.geo@gmail.com>
2024-10-27 00:23:58 -0400

    Fix: Resolve issue with nested short URLs
    Updates the `path` configuration for the "Short" collection to correctly handle nested URLs, ensuring consistent retrieval and rendering of short-form content.
    
    Fix: Handle nested URLs for short-form content
    
    Updates the Keystatic configuration to support nested URLs for the "Short" collection. This addresses an issue where content with nested URLs was not being fetched or rendered correctly, ensuring consistent delivery of short-form content.

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,4 +1,5 @@
 // keystatic.config.ts
+import Bookmark from '@components/Bookmark.astro';
 import { config, fields, collection } from '@keystatic/core';
 
 const REPO_OWNER = 'jgeofil';
@@ -7,18 +8,30 @@ const REPO_NAME = 'mlreadme-astro-blog';
 
 export default config({
     storage: {
-        kind: 'github',
-        repo: `${REPO_OWNER}/${REPO_NAME}`
+        kind: 'local',//'github',
+        //repo: `${REPO_OWNER}/${REPO_NAME}`
     },
   collections: {
-    posts: collection({
-      label: 'Short',
-      slugField: 'title',
-      path: 'src/data/shorts/*',
+    blog: collection({
+      label: 'Blog',
+      slugField: 'id',
+      path: 'src/data/blog/*',
       format: { contentField: 'content' },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.mdx({ label: 'Content' }),
+        id: fields.slug({ name: { label: 'ID' } }),
+        title: fields.text({ label: 'Title' }),
+        description: fields.text({ label: 'Description' }),
+        pubDate: fields.date({ label: 'Publication Date' }),
+        updatedDate: fields.date({ label: 'Updated Date' }),
+        sections: fields.text({ label: 'Sections' }),
+        tags: fields.array(fields.text({ label: 'Tags' })),
+        cover: fields.object({
+          alt: fields.text({ label: 'Alt' }),
+          src: fields.text({ label: 'Src' })
+        }),
+        content: fields.mdx({ 
+          label: 'Content'
+        }),
       },
     }),
   },
